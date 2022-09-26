d8fcac637a605243bf2098b262d83df05c1fafdd
Ludovico Leardi <ludovico.leardi@itiscuneo.eu>
2022-09-26 13:03:03 +0200

    Esercizi 1 pag 55 e 3 pag 55

---
+++ es_1.py
@@ -0,0 +1,16 @@
+from re import A
+import string
+
+a = 3
+frase = "ciao"
+b = 3.2
+c = True
+
+print(a)
+print(type(a))
+print(frase)
+print(type(frase))
+print(b)
+print(type(b))
+print(c)
+print(type(c))
---
+++ es_2.py
@@ -0,0 +1,12 @@
+prima, seconda = 2, 3
+
+"""stampo i valori"""
+print(prima, seconda)
+
+"""scambio i valori """
+tmp = prima
+prima = seconda
+seconda = tmp
+
+"""stampo i valori scambiati"""
+print(prima, seconda)
